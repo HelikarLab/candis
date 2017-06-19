7435ecce8b5b15b22ff5f9592be79ef6e5621c6a
achillesrasquinha <achillesrasquinha@gmail.com>
2017-06-19 12:42:44 +0530

    updated package.py

--- package.py
+++ package.py
@@ -1,10 +1,11 @@
-# Inspired by npm's package.json file
 name              = 'candis'
 version           = '0.1.0'
 release           = '0.1.0'
 description       = 'A data mining suite for DNA microarrays'
 long_description  = ['README.md']
-keywords          = [ ]
+keywords          = ['data', 'mining', 'suite', 'bioinformatics', 'machine',
+    'learning'
+]
 authors           = [
     { 'name': 'Achilles Rasquinha', 'email': 'achillesrasquinha@gmail.com' }
 ]
@@ -13,34 +14,12 @@ maintainers       = [
 ]
 license           = 'GNU GPL v3'
 modules           = [
-    'candis',
 
-    'candis.app',
-    'candis.app.client',
-    'candis.app.server',
-    'candis.app.server.api',
-
-    'candis.cli',
-
-    'candis.config',
-
-    'candis.data',
-    'candis.data.cdata',
-    'candis.data.entrez',
-
-    'candis.manager',
-    'candis.manager.cache',
-
-    'candis.resource',
-
-    'candis.exception',
-
-    'candis.util'
 ]
 test_modules      = [
-    'bulbea.util.test'
+
 ]
-homepage          = 'https://achillesrasquinha.github.io/candis'
+homepage          = 'https://candis.readthedocs.io'
 github_username   = 'achillesrasquinha'
 github_repository = 'candis'
 github_url        = '{baseurl}/{username}/{repository}'.format(
