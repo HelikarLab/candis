37fd50219e67dd5acdc90e76238dcbf719bfbe38
rupav jain <rupavwinchester@gmail.com>
2018-05-15 23:11:52 +0530

    Travis setup successful (#42)
    * travis build success
    
    * modified indentation

--- package.py
+++ package.py
@@ -5,7 +5,10 @@ import io
 from   setuptools import find_packages
 
 # imports - third-party imports
-from   pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 # Python 2
 try:
