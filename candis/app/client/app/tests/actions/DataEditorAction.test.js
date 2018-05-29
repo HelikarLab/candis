84cd042705bc4cfa0d14e8d10f9eba5e46eaf0b5
rupav <rupavwinchester@gmail.com>
2018-05-29 11:53:37 +0530

    Add DataEditorAction test

--- candis/app/client/app/tests/actions/DataEditorAction.test.js
+++ candis/app/client/app/tests/actions/DataEditorAction.test.js
@@ -1,58 +1,67 @@
 import ActionType from '../../constant/ActionType'
-import { row, column } from '../../action/DataEditorAction'
+import { row as row_, column as column_ } from '../../action/DataEditorAction'
+
+const position = 1
+const row = 1
+const index = 1
+const column = 1
+const key = 'CEL'
+const from = 1
+const to = 2
+const update = ['Normal', 'Male']
 
 test('should setup row.insert action object', () => {
-    const action = row.insert(position = 1, row = 2)
+    const action = row_.insert(position, row)
     expect(action).toEqual({
         type: ActionType.DataEditor.INSERT_ROW,
-        payload: { position: position, row: row }
+        payload: { position, row }
     })
 })
 
 test('should setup row.select action object', () => {
-    const action = row.select()
+    const action = row_.select(index, row)
     expect(action).toEqual({
         type: ActionType.DataEditor.SELECT_ROW,
-        payload: 
+        payload: { index, row }
     })
 })
 
 test('should setup row.update action object', () => {
-    const action = row.update()
+    const action = row_.update( from, to, update )
     expect(action).toEqual({
         type: ActionType.DataEditor.UPDATE_ROW,
-        payload: 
+        payload: { from, to, update }
     })
 })
 
 test('should setup row.deselect action object', () => {
-    const action = row.deselect()
+    const action = row_.deselect(index, row)
     expect(action).toEqual({
         type: ActionType.DataEditor.DESELECT_ROW,
-        payload: 
+        payload: { index, row }
     })
 })
 
 test('should setup row.delete action object', () => {
-    const action = row.delete()
+    const action = row_.delete(index, row)
     expect(action).toEqual({
         type: ActionType.DataEditor.DELETE_ROW,
-        payload: ''
+        payload: { index, row }
     })
 })
 
 test('should setup column.delete action object', () => {
-    const action = column.insert()
+    const action = column_.insert(position, column)
     expect(action).toEqual({
         type: ActionType.DataEditor.INSERT_COLUMN,
-        payload: 
+        payload: {position, column}
     })
 })
 
 test('should setup column.delete action object', () => {
-    const action = column.delete()
+    const action = column_.delete(key)
     expect(action).toEqual({
         type: ActionType.DataEditor.DELETE_COLUMN,
-        payload: 
+        payload: key
     })
 })
