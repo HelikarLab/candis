46da685de6558856f3969f203328c3700256b6c7
rupav jain <rupavwinchester@gmail.com>
2018-06-02 19:51:18 +0530

    Complete test cases for PipelineAction, add axios-mock-adapter to mock axios. (#73)
    * Add axios-mock-adapter for testing axios
    
    * Add test to check pipeine.delete action object
    
    * Completes test cases for PipelineAction

--- candis/app/client/app/tests/actions/PipelineAction.test.js
+++ candis/app/client/app/tests/actions/PipelineAction.test.js
@@ -1,20 +1,103 @@
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
 
+import config from '../../config'
 import pipeline from '../../action/PipelineAction'
 import activePipe from '../fixtures/pipeline'
+import ActionType from '../../constant/ActionType'
+import dokuments from '../fixtures/documents'
+
 
 const createMockStore = configureMockStore([thunk])
+const mock = new MockAdapter(axios)
 
-// To be Implemented - maybe mock `axios` library. Currently getting stuck in Asynch calls
- test('should setup pipeline.delete action object ', (/*done*/) => {
-//     const store = createMockStore({})
-//     store.dispatch(pipeline.delete("testPipe.cpipe")).then(() => {
-//         const actions = store.getActions()
-//         expect(actions[0]).toEqual({
-//             type: 'DELETE'
-//         })
-//         done()   
-//     })
+ test('should setup pipeline.delete action object ', (done) => {
+    const data = { data: 'true' }  // dummy data object for DELETE_SUCCESS action.
+    mock.onPost(config.routes.API.data.delete, { name: activePipe }).reply(200, data)
+    const store = createMockStore({})
+    store.dispatch(pipeline.delete(activePipe)).then(() => {
+        
+        const actions = store.getActions()
+        expect(actions[0]).toEqual({
+            type: ActionType.Asynchronous.DELETE_SUCCESS,
+            payload: {
+                ...data,
+                name: activePipe
+            }
+        })
+        expect(actions[1]).toEqual({
+            type: ActionType.Pipeline.DELETE_PIPELINE
+        })
+        done()
+    })
     
+ })
+
+ test('should catch error while trying to delete pipeline using axios', (done) => {
+    const code = 404  // should be greater than or equal to 400
+    const error = new Error(`Request failed with status code ${code}`)  
+    
+    mock.onPost(config.routes.API.data.delete, { name: activePipe }).reply(code, error)
+    const store = createMockStore({})
+    store.dispatch(pipeline.delete(activePipe)).then(() => {
+        
+        const actions = store.getActions()
+        expect(actions[0]).toEqual({
+            type: ActionType.Asynchronous.DELETE_ERROR,
+            payload: {
+                name: activePipe,
+                error
+            }
+        })
+        done()
+
+    })
+ })
+
+ test('should setup pipeline.run action object', (done) => {
+    const data = { data: true }  // dummy data which is returned by successful axios call.
+    mock.onPost(config.routes.API.pipeline.run, dokuments.active).reply(200, data)
+    const store = createMockStore({})
+
+    store.dispatch(pipeline.run(dokuments.active)).then(() => {
+
+        const actions = store.getActions()
+        expect(actions[0]).toEqual({
+            type: ActionType.Pipeline.RUN_REQUEST,
+			payload: dokuments.active
+        })
+        expect(actions[1]).toEqual({
+            type: ActionType.Pipeline.RUN_SUCCESS,
+			payload: {
+                output: dokuments.active,
+                ...data
+            }
+        })
+        done()
+    })
+ })
+
+ test('should catch error while trying to run the pipeline', (done) => {
+    const store = createMockStore({})
+    const code = 404  // should be greater than or equal to 400
+    const error = new Error(`Request failed with status code ${code}`)  // dummy error
+
+    mock.onPost(config.routes.API.pipeline.run, dokuments.active).reply(code, error)
+    store.dispatch(pipeline.run(dokuments.active)).then(() => {
+        const actions = store.getActions()
+        expect(actions[0]).toEqual({
+            type: ActionType.Pipeline.RUN_REQUEST,
+			payload: dokuments.active
+        })
+        expect(actions[1]).toEqual({
+            type: ActionType.Pipeline.RUN_ERROR,
+            payload: {
+                output: dokuments.active,
+                error: undefined  // In PipelineAction, instead of "error", "response" is destructured
+            }
+        })
+    })
+    done()
  })
--- candis/app/client/app/tests/reducers/DocumentProcessorReducer.test.js
+++ candis/app/client/app/tests/reducers/DocumentProcessorReducer.test.js
@@ -1,4 +1,6 @@
 import documentProcessor from '../../reducer/DocumentProcessorReducer'
+import ActionType from '../../constant/ActionType'
+import dokuments from '../fixtures/documents'
 
 const initial             =
 {
@@ -15,4 +17,20 @@ test('should setup initial state of DocumentProcessorReducer', () => {
     expect(state).toEqual({
         ...initial
     })
+})
+
+test('should setup ', () => {
+    // const state = documentProcessor(
+    //     undefined,
+    //     {
+    //         type: ActionType.Asynchronous.READ_SUCCESS,
+    //         payload: dokuments.active
+    // })
+    // expect(state).toEqual({
+    //     ...dokuments,
+    //     "active": {
+    //         ...dokuments.active,
+    //         "ID": expect.any(String),
+    //     },
+    // })
 })
