94d3f701de4e8c3886fdf06b5f320f0361311d3f
rupav <rupavwinchester@gmail.com>
2018-06-04 14:32:24 +0530

    Add test case for documentProcessorReducer
    Add documentProcessorReducer test
    
    Improve fixtures
    
    Add test case for documentProcessorReducer

--- candis/app/client/app/tests/reducers/DocumentProcessorReducer.test.js
+++ candis/app/client/app/tests/reducers/DocumentProcessorReducer.test.js
@@ -1,6 +1,6 @@
 import documentProcessor from '../../reducer/DocumentProcessorReducer'
 import ActionType from '../../constant/ActionType'
-import dokuments from '../fixtures/documents'
+import dokuments, { newDoc } from '../fixtures/documents'
 
 const initial             =
 {
@@ -19,18 +19,48 @@ test('should setup initial state of DocumentProcessorReducer', () => {
     })
 })
 
-test('should setup ', () => {
-    // const state = documentProcessor(
-    //     undefined,
-    //     {
-    //         type: ActionType.Asynchronous.READ_SUCCESS,
-    //         payload: dokuments.active
-    // })
-    // expect(state).toEqual({
-    //     ...dokuments,
-    //     "active": {
-    //         ...dokuments.active,
-    //         "ID": expect.any(String),
-    //     },
-    // })
+test('should return state with one object being active given ONE ducument matches the provided state', () => {
+    const state = documentProcessor(
+        dokuments,
+        {
+            type: ActionType.Asynchronous.READ_SUCCESS,
+            payload: dokuments.active
+    })
+     
+    expect(state).toEqual({
+        ...dokuments,
+        active: {
+            ...dokuments.active,
+            data: expect.any(Array)
+        }
+    })
+    state.active.data.forEach(node => {
+        expect(Object.keys(node).sort()).toEqual(
+            ['ID', 'code', 'name', 'label', 'value', 'status'].sort()
+        )
+    })
+
 })
+
+test('should return state with provided doc being active given NO state.document matches the provided doc', () => {
+    const state = documentProcessor(
+        dokuments,
+        {
+            type: ActionType.Asynchronous.READ_SUCCESS,
+            payload: newDoc
+    })
+    expect((dokuments.documents).length + 1).toBe(state.documents.length)  // with new doc, size should increase by 1
+    // check if active doc is changed
+    expect(state.active).toEqual({
+        ...newDoc,
+        ID: expect.any(String),
+        data: expect.any(Array),
+    })
+    state.active.data.forEach(node => {
+        expect(Object.keys(node).sort()).toEqual(
+            ['ID', 'code', 'name', 'label', 'value', 'status'].sort()
+        )
+    })
+})
+
+// TO be implemented; test for ActionType.Asynchronous.WRITE_SUCCESS for this reducer
