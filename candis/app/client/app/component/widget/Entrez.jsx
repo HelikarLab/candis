a76fac70db2a70064cc298c4e13c1f40f5d4fac7
rupav <rupavwinchester@gmail.com>
2018-06-06 02:36:41 +0530

    Download data WIP

--- candis/app/client/app/component/widget/Entrez.jsx
+++ candis/app/client/app/component/widget/Entrez.jsx
@@ -5,86 +5,74 @@ import { withFormik, Form, Field } from 'formik'
 
 import * as Yup from 'yup'
 
-class EntrezBasic extends React.Component {
-    constructor (props) {
-        super(props)
-        this.state = undefined
-    }
 
-
-    render() {
-        const props = this.props
-        return (
-            <div className={classNames("panel panel-default", props.classNames.root)}>
-                <div className="panel-body">
-                
-                    <Form>
-                        <div className="row">
-                            <div className="col-xs-3">
-                                <label>Email</label>
-                            </div>
-                            <div className="col-xs-6">
-                                <Field type="email" name="email"/>
-                            </div>
-                            <div className="col-xs-3">
-                                {props.touched.email && props.errors.email && <small>{props.errors.email}</small>}
-                            </div>
+const EntrezBasic = (props) => {
+    return (
+        <div className={classNames("panel panel-default", props.classNames.root)}>
+            <div className="panel-body">
+                <Form>
+                    <div className="row">
+                        <div className="col-xs-3">
+                            <label>Email</label>
                         </div>
-
-                        <div className="row">
-                            <div className="col-xs-6">
-                                <label>Tool Name</label>
-                            </div>
-                            <div className="col-xs-6">
-                                <Field type="text" name="toolName"/>
-                            </div>
-                        </div>
-
-                        <div className="row">
-                            <div className="col-xs-6">
-                                <label>API KEY</label>
-                            </div>
-                            <div className="col-xs-6">
-                                <Field type="text" name="api_key"/>
-                            </div>
-                        </div>
-
-                        <div className="row">
-                            <div className="col-xs-6">
-                                <label>Database</label>
-                            </div>
-                            <div className="col-xs-6">
-                                <Field component="select" name="database">
-                                    <ul>
-                                    <li><option value="1">1</option></li>
-                                    <option value="2">2</option>
-                                    </ul>
-                                </Field>
-
-                            </div>
-                        </div>
-
-                        <div className="row">
-                            <div className="col-xs-6">
-                                <label>Term</label>
-                            </div>
-                            <div className="col-xs-6">
-                                <Field type="text" name="term" placeholder="Term"/>
-                            </div>
+                        <div className="col-xs-6">
+                            <Field type="email" name="email"/>
                         </div>
                         <div className="col-xs-3">
-                            {props.touched.term && props.errors.term && <small>{props.errors.term}</small>}
+                            {props.touched.email && props.errors.email && <small>{props.errors.email}</small>}
                         </div>
-                        <button>Search</button>
-                    
-                    </Form>
-                
-                </div>
-            </div>
-        )
-    }
+                    </div>
 
+                    <div className="row">
+                        <div className="col-xs-6">
+                            <label>Tool Name</label>
+                        </div>
+                        <div className="col-xs-6">
+                            <Field type="text" name="toolName"/>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-xs-6">
+                            <label>API KEY</label>
+                        </div>
+                        <div className="col-xs-6">
+                            <Field type="text" name="api_key"/>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-xs-6">
+                            <label>Database</label>
+                        </div>
+                        <div className="col-xs-6">
+                            <Field component="select" name="database">
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                            </Field>
+
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-xs-6">
+                            <label>Term</label>
+                        </div>
+                        <div className="col-xs-6">
+                            <Field type="text" name="term" placeholder="Term"/>
+                        </div>
+                    </div>
+                    <div className="col-xs-3">
+                        {props.touched.term && props.errors.term && <small>{props.errors.term}</small>}
+                    </div>
+                    <button disabled={props.isSubmitting}>Search</button>
+                </Form>
+            </div>
+        </div>
+    )
 }
+
+
 
 const Entrez = withFormik({
     mapPropsToValues({ email, toolName, database, term, useHistory, api_key }){
