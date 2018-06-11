19277c3b1f36b5449d04c6360552c11ed29ae256
rupav jain <rupavwinchester@gmail.com>
2018-06-11 10:47:10 +0530

    Enhanced 2 reducers for 100% coverage (#86)

--- candis/app/client/app/tests/reducers/AppReducer.test.js
+++ candis/app/client/app/tests/reducers/AppReducer.test.js
@@ -5,6 +5,27 @@ const initial = {
     user: null
 }
 
+const dummyApp = {
+    user: 'rupav'
+}
+
+const ActionTypeList = {
+    setZero: [
+        ActionType.App.SIGNIN_REQUEST,
+        ActionType.App.SIGNOUT_REQUEST
+    ],
+    setOne: [
+        ActionType.App.SIGNIN_SUCCESS,
+        ActionType.App.SIGNIN_ERROR,
+        ActionType.App.SIGNOUT_SUCCESS,
+        ActionType.App.SIGNOUT_ERROR,
+        ActionType.Asynchronous.WRITE_SUCCESS,
+        ActionType.Asynchronous.WRITE_ERROR,
+        ActionType.Asynchronous.GET_RESOURCE_SUCCESS,
+        ActionType.Asynchronous.GET_RESOURCE_ERROR
+    ]
+}
+
 test('should setup AppReducer with default state', () => {
     const state = AppReducer(
         undefined,
@@ -17,35 +38,39 @@ test('should setup AppReducer with default state', () => {
     })
 })
 
-test('should return provided state and set nprogress to 0 for action Type: SIGNIN_REQUEST', () => {
-    const state = AppReducer(
-        initial,
-            {
-            payload: null,
-            type: ActionType.App.SIGNIN_REQUEST
-        }
-    )
-    expect(nprogress.set).toHaveBeenCalledWith(0.0)
-    expect(state).toEqual({
-        ...initial
+test('should return provided state and set nprogress to 0.0', () => {
+    ActionTypeList.setZero.forEach(ActionTypeItem => {
+        const state = AppReducer(
+            dummyApp,
+                {
+                    payload: null,
+                    type: ActionTypeItem
+            }
+        )
+        expect(state).toEqual({
+            ...dummyApp
+        })
+        expect(nprogress.set).toHaveBeenCalledWith(0.0)
     })
 })
 
-test('should return provided state and set nprogress to 0 for action Type: SIGNOUT_REQUEST', () => {
-    const state = AppReducer(
-        initial,
-            {
-            payload: null,
-            type: ActionType.App.SIGNOUT_REQUEST
-        }
-    )
-    expect(nprogress.set).toHaveBeenCalledWith(0.0)
-    expect(state).toEqual({
-        ...initial
+test('should return provided state and set nprogress to 1.0', () => {
+    ActionTypeList.setOne.forEach(ActionTypeItem => {
+        const state = AppReducer(
+            dummyApp,
+                {
+                    payload: null,
+                    type: ActionTypeItem
+            }
+        )
+        expect(state).toEqual({
+            ...dummyApp
+        })
+        expect(nprogress.set).toHaveBeenCalledWith(1.0)
     })
 })
 
-test('should modify provided state and nprogress to 0 for action Type: SIGNOUT_REQUEST', () => {
+test('should modify the provided state', () => {
     const fakeUser = 'RJ'
     const state = AppReducer(
         undefined,
