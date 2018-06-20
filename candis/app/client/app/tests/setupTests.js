3a04d49675fab4a1e0b426add394983929752f84
rupav <rupavwinchester@gmail.com>
2018-06-21 03:07:35 +0530

    Remove enzyme-preact adapter

--- candis/app/client/app/tests/setupTests.js
+++ candis/app/client/app/tests/setupTests.js
@@ -1,9 +1,3 @@
-import Enzyme from 'enzyme'
-import { Adapter } from 'enzyme-adapter-preact'
 import nprogress from 'nprogress'
-
-Enzyme.configure({
-    adapter: new Adapter()
-})
 
 global.nprogress = nprogress
