0aaf2600c30ee6ef61f4b2321bcfa10e4e492b1a
rupav jain <rupavwinchester@gmail.com>
2018-06-27 11:48:25 +0530

    Add smoke tests, snapshot testing and action test cases (#105)

---
+++ candis/app/client/app/tests/reducers/index.test.js
@@ -0,0 +1,12 @@
+import { createStore } from 'redux'
+
+import Reducer from '../../reducer/index'
+
+// reference: https://github.com/reduxjs/redux/issues/1412
+test('smoke test: expect store created using combineReducers should contain given reducers', () => {
+    const store = createStore(Reducer)
+    expect(Object.keys(store.getState()).sort()).toEqual(
+        ['app', 'toolBox', 'documentProcessor', 'modal', 'dataEditor', 'data'].sort()
+    )
+})
+
--- candis/app/client/app/tests/setupTests.js
+++ candis/app/client/app/tests/setupTests.js
@@ -1,3 +1,6 @@
+import { configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 import nprogress from 'nprogress'
 
+configure({ adapter: new Adapter() })
 global.nprogress = nprogress
