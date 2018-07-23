6bd7ba8f78ac1275e135d04c8631f1d7b02a5f34
rupav jain <rupavwinchester@gmail.com>
2018-07-23 23:48:50 +0530

    Add a feature to split ARFF data into training and testing given a pe…  …rcentage (#144)
    * Add option in backend to split ARFF file into training and testing dataset
    
    * Add a feature to split ARFF data into training and testing given a percentage

--- candis/app/server/api/pipeline.py
+++ candis/app/server/api/pipeline.py
@@ -26,7 +26,7 @@ from candis.app.server.models import PipelineRun, Pipeline as PipelineModel, Cda
 # HINT: Can be written better?
 @app.route(CONFIG.App.Routes.API.Pipeline.RUN, methods = ['POST'])
 @login_required
-def run(delay = 5):
+def run(delay = 5, split_percent = 30):
     response         = Response()
 
     parameters       = addict.Dict(request.get_json())
@@ -59,7 +59,7 @@ def run(delay = 5):
                 opath =  os.path.join(fpath.value.path, fpath.value.name)
                 cdat = CData.load_from_json(json.loads(cdat_dict), opath)
 
-                pipe.run(cdat, verbose = CONFIG.DEBUG)
+                pipe.run(cdat, verbose = CONFIG.DEBUG, split_percent = split_percent)
 
                 while pipe.status == Pipeline.RUNNING:
                     status        = addict.Dict()
