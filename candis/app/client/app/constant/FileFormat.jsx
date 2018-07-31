3a940071e6115f5a0e78996680aa8fa132676092
rupav jain <rupavwinchester@gmail.com>
2018-07-31 13:04:28 +0530

    call the predict endpoint after the given user selections. (#151)
    * Complete the predict button's function in front end
    
    * remove comments

--- candis/app/client/app/constant/FileFormat.jsx
+++ candis/app/client/app/constant/FileFormat.jsx
@@ -1,10 +1,12 @@
 const FileFormat =
 {
+      ARFF: 'arff',
      CDATA: 'cdata',
      	 CSV: 'csv',
        CEL: 'cel',
   PIPELINE: 'pipeline',
-      GIST: 'gist'
+      GIST: 'gist',
+     MODEL: 'model'
 };
 
 export default FileFormat
