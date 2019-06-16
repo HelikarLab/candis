d0bf2a7bc4de32e5dec92724d857d256de8f35e6
Harshit Khetan <khetanmehul@gmail.com>
2019-06-17 00:26:48 +0530

    Add webpack dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,19 @@
-var path           = require('path')
-,   webpack        = require('webpack')
-,   UglifyJSPlugin = require('uglifyjs-webpack-plugin')
-,   Jarvis         = require('webpack-jarvis');
+var path = require('path'),
+  webpack = require('webpack'),
+  UglifyJSPlugin = require('uglifyjs-webpack-plugin'),
+  Jarvis = require('webpack-jarvis');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 require('dotenv').config()
 
-var paths          = { };
-paths.BASE         = path.join(__dirname, 'candis', 'app');
-paths.APP          = path.join(paths.BASE, 'client', 'app');
+var paths = {};
+paths.BASE = path.join(__dirname, 'candis', 'app');
+paths.APP = path.join(paths.BASE, 'client', 'app');
 
-var config         = require(path.join(paths.APP, 'config'));
+var config = require(path.join(paths.APP, 'config'));
 
-module.exports     = {
+module.exports = {
+  mode: 'development',
   entry: [
     path.join(paths.APP, 'Client.jsx'),
     path.join(paths.APP, 'plugins.js')
@@ -19,43 +21,39 @@ module.exports     = {
   output: {
     path: path.join(paths.BASE, 'assets', 'js'),
     filename: 'bundle.min.js',
-    publicPath: 'http://' + config.host + ':' + config.port
+    publicPath: path.join(paths.BASE, 'assets', 'js')
   },
   module: {
-    rules: [
-      {
-           test: /\.(js|jsx)$/,
-         loader: 'babel-loader',
-        exclude: /(node_modules)/
-      },
-      {
-          test: /\.json$/,
-        loader: 'json-loader'
-      }
-    ]
+    rules: [{
+      test: /\.(js|jsx)$/,
+      loader: 'babel-loader',
+      exclude: /(node_modules)/
+    }]
   },
-  plugins: process.env.ENV === 'development' ?
-    [
-      // debug plugins go here.
-      new Jarvis(),
-      new webpack.DefinePlugin({
-      	'process.env': {
-		'NODE_ENV': JSON.stringify('development')
-      	}
-      })
-    ]
-    :
-    [
-      // production plugins go here.
-      new UglifyJSPlugin({
-        uglifyOptions: {
-          beautify: false,
-          ecma: 6,
-          compress: true,
-          comments: false
-        }
-      })
-    ],
+  plugins: process.env.NODE_ENV === 'development' ? [
+    // debug plugins go here.
+    new Jarvis(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('development')
+      }
+    }),
+    new HtmlWebpackPlugin({
+      title: "Candis App",
+      filename: "../index.html",
+      inject: true
+    }),
+  ] : [
+    // production plugins go here.
+    new UglifyJSPlugin({
+      uglifyOptions: {
+        beautify: false,
+        ecma: 6,
+        compress: true,
+        comments: false
+      }
+    })
+  ],
   resolve: {
     modules: ['node_modules'],
     extensions: ['.js', '.jsx', '.json']
