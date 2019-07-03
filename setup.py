d07b0a5a410baf36fd9448ea1808d28a85132b73
harshit khetan <khetanmehul@gmail.com>
2019-07-04 01:05:56 +0530

    Update setup.py script

--- setup.py
+++ setup.py
@@ -10,87 +10,94 @@ from setuptools import setup, find_packages
 import pip
 
 try:
-    from pip._internal.req import parse_requirements # pip 10
+    from pip._internal.req import parse_requirements  # pip 10
 except ImportError:
-    from pip.req           import parse_requirements # pip 9
+    from pip.req import parse_requirements  # pip 9
 
 # globals
-PACKAGE     = "candis"
-SRCDIR      = "src"
+PACKAGE = "candis"
+SRCDIR = "src"
+
 
 def isdef(var):
     return var in globals()
 
-def read(path, encoding = None):
+
+def read(path, encoding=None):
     content = None
-    
-    with io.open(path, encoding = encoding) as f:
+
+    with io.open(path, encoding=encoding) as f:
         content = f.read()
 
     return content
 
+
 def get_package_info():
     attr = osp.abspath(osp.join(SRCDIR, PACKAGE, "__attr__.py"))
-    info = dict(__file__ = attr) # HACK
-    
+    info = dict(__file__=attr)  # HACK
+
     with open(attr) as f:
         content = f.read()
         exec(content, info)
 
     return info
 
-def get_dependencies(type_ = None):
-    path         = osp.realpath("requirements.txt")
-    requirements = [str(ir.req) for ir in parse_requirements(path, session = "hack")]
+
+def get_dependencies(type_=None):
+    path = osp.realpath("requirements.txt")
+    requirements = [str(ir.req)
+                    for ir in parse_requirements(path, session="hack")]
 
     if type_ == "development":
-        path         = osp.realpath("requirements-dev.txt")
+        path = osp.realpath("requirements-dev.txt")
         requirements = [
-            str(ir.req) for ir in parse_requirements(path, session = "hack")
-                if str(ir.req) not in requirements
+            str(ir.req) for ir in parse_requirements(path, session="hack")
+            if str(ir.req) not in requirements
         ]
-    
+
     return requirements
 
-PKGINFO    = get_package_info()
+
+PKGINFO = get_package_info()
 
 setup(
-    name                 = PKGINFO["__name__"],
-    version              = PKGINFO["__version__"],
-    url                  = PKGINFO["__url__"],
-    author               = PKGINFO["__author__"],
-    author_email         = PKGINFO["__email__"],
-    description          = PKGINFO["__description__"],
-    long_description     = read("README.md", encoding = "utf8"),
-    long_description_content_type = "text/markdown",
-    license              = PKGINFO["__license__"],
-    keywords             = " ".join(PKGINFO["__keywords__"]),
-    packages             = find_packages(where = SRCDIR),
-    package_dir          = { "": SRCDIR },
-    zip_safe             = False,
-    entry_points         = {
+    name=PKGINFO["__name__"],
+    version=PKGINFO["__version__"],
+    url=PKGINFO["__url__"],
+    author=PKGINFO["__author__"],
+    author_email=PKGINFO["__email__"],
+    description=PKGINFO["__description__"],
+    long_description=read("README.md", encoding="utf8"),
+    long_description_content_type="text/markdown",
+    license=PKGINFO["__license__"],
+    keywords=" ".join(PKGINFO["__keywords__"]),
+    packages=find_packages(where=SRCDIR),
+    package_dir={"": SRCDIR},
+    zip_safe=False,
+    entry_points={
         "console_scripts": [
             "%s = %s.__main__:main" % (
-                PKGINFO["__command__"] if hasattr(PKGINFO, "__command__") else PKGINFO["__name__"],
+                PKGINFO["__command__"] if hasattr(
+                    PKGINFO, "__command__") else PKGINFO["__name__"],
                 PACKAGE
             )
         ]
     },
-    
-    install_requires     = get_dependencies(type_ = "production"),
-    extras_require       = dict(
-        dev = get_dependencies(type_ = "development")
+
+    install_requires=get_dependencies(type_="production"),
+    extras_require=dict(
+        dev=get_dependencies(type_="development")
     ),
-    include_package_data = True,
-    classifiers          = [
+    include_package_data=True,
+    classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
-        
+
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
-        
+
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
--- src/candis/__attr__.py
+++ src/candis/__attr__.py
@@ -1,5 +1,81 @@
-# module - candis
-from candis.config import CONFIG
+import sys
+import os
+import os.path as osp
+import subprocess
 
-__version__ = CONFIG.VERSION
-__release__ = CONFIG.RELEASE
+PY2 = sys.version_info.major == 2
+
+try:
+    FileNotFoundError
+except NameError:
+    if PY2:
+        FileNotFoundError = OSError
+
+
+def read(fname):
+    with open(fname) as f:
+        data = f.read()
+    return data
+
+
+def pardir(fname, level=1):
+    for _ in range(level):
+        fname = osp.dirname(fname)
+    return fname
+
+
+def strip(string):
+    string = string.lstrip()
+    string = string.rstrip()
+    return string
+
+
+def safe_decode(object_, encoding="utf-8"):
+    decoded = object_
+
+    try:
+        decoded = object_.decode(encoding)
+    except AttributeError:
+        pass
+
+    return decoded
+
+
+def sequence_filter(list_, filter_, type_=list):
+    result = type_(filter(filter_, list_))
+    return result
+
+
+def get_revision(path, short=False, raise_err=True):
+    """
+    Returns the git revision of a repository. Raises error if not a valid git repository.
+    """
+    revision = None
+
+    try:
+        short = "--short" if short else ""
+        with open(os.devnull, "w") as NULL:
+            output = subprocess.check_output(sequence_filter(["git", "rev-parse", short, "HEAD"], filter_=None),
+                                             stderr=NULL, cwd=path)
+        revision = safe_decode(strip(output))
+    except (subprocess.CalledProcessError, FileNotFoundError):
+        if raise_err:
+            raise
+
+    return revision
+
+
+path = dict()
+path["base"] = pardir(__file__)
+path["version"] = osp.join(path["base"], "VERSION")
+
+__name__ = "candis"
+__command__ = "candis"
+__version__ = strip(read(path["version"]))
+__build__ = get_revision(pardir(path["base"], 2), short=True, raise_err=False)
+__url__ = "https://github.com/HelikarLab/candis"
+__author__ = "HelikarLab Developers"
+__email__ = "hello.helikarlab@gmail.com"
+__description__ = "A data mining suite for gene expression data"
+__keywords__ = ["data", "mining", "suite", "gene", "expression", "data"]
+__license__ = "MIT"
